4c2ded7dbf12e19a587d1822be0c0e8aaab5d57f
Frahaan Hussain <Sonar Systems>
2014-09-17 06:47:47 +0100

    Positioning Using MoveTo

---
+++ src/app.js
@@ -0,0 +1,34 @@
+
+var HelloWorldLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+
+        var sprite = new cc.Sprite.create(res.CloseNormal_png);
+        sprite.setAnchorPoint(cc.p(0.5, 0.5));
+        sprite.setPosition(cc.p(size.width / 2, size.height / 2));
+        this.addChild(sprite, 0);
+
+        var sprite_action = cc.MoveTo.create(2, cc.p(50, 100));
+        sprite.runAction(sprite_action);
+
+        return true;
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
